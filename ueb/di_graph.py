b33c2269ba9484f3a8c9de7dd021cd6a42c2b86b
r41L <stephanbrummel@online.de>
2017-04-12 22:09:29 +0200

    class di_graph out of graph.py

---
+++ ueb/di_graph.py
@@ -0,0 +1,25 @@
+from ueb.graph import Graph
+
+class DiGraph(Graph):
+    def __init__(self, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
+
+    def toGraph(self, allow_multi=True):
+        self.dot = gv.Digraph(
+            name=self.title,
+            directory="graphs",
+            filename=self.filename,
+            strict=not allow_multi,
+            graph_attr={},
+            node_attr={},
+            edge_attr={}
+        )
+        self.__buildDot()
+        return self.dot
+
+    # ----------------------------------------------------------------
+    # not supported methods
+
+    def hierholzer(self):
+        print("Method hierholzer not supported for directed graphs")
+        return
--- ueb/graph.py
+++ ueb/graph.py
@@ -177,28 +177,3 @@ class Graph:
         if self.adjazenz.pop(node_to_delete, False):
             for node in self.adjazenz.keys():
                 self.removeEdge(node, node_to_delete)
-
-
-class DiGraph(Graph):
-    def __init__(self, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
-
-    def toGraph(self, allow_multi=True):
-        self.dot = gv.Digraph(
-            name=self.title,
-            directory="graphs",
-            filename=self.filename,
-            strict=not allow_multi,
-            graph_attr={},
-            node_attr={},
-            edge_attr={}
-        )
-        self.__buildDot()
-        return self.dot
-
-    # ----------------------------------------------------------------
-    # not supported methods
-
-    def hierholzer(self):
-        print("Method hierholzer not supported for directed graphs")
-        return
