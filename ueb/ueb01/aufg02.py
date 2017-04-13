e503e1ed35c4059c1477f7717f820e331fb57ba4
ToTenTiger <bjoern.nowak@web.de>
2017-04-13 18:31:17 +0200

    adaption to new structure

--- ueb/ueb01/aufg02.py
+++ ueb/ueb01/aufg02.py
@@ -2,7 +2,8 @@ from ueb.graph import Graph
 
 
 graph = Graph(title="Excise 01 graph 02",
-              filename="01-02")
+              filename="01-02",
+              allow_multi=True)
 graph.read_input(value="0:2,3,5,7;" +
                        "1:3,4,5,6;" +
                        "2:0,6,7,9;" +
@@ -15,7 +16,7 @@ graph.read_input(value="0:2,3,5,7;" +
                        "9:2,6,7,10;" +
                        "10:3,5,7,9;;")
 
-dot = graph.to_graph(allow_multi=True)
+dot = graph.to_graph()
 #print(dot.source)
-dot.render(view=0, cleanup=1)
+dot.render(view=1, cleanup=1)
 print("Hierholzer Output: {}".format(graph.hierholzer()))
