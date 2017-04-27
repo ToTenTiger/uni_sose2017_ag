475dbe1dda2c851d57b0d88e0e531140d4bb89d3
r41l <stephanbrummel@online.de>
2017-04-27 09:23:49 +0200

    Updated Nodes (methods around weight)

--- classes/node.py
+++ classes/node.py
@@ -1,31 +1,39 @@
-from itertools import groupby
-from heapq import *
-from typing import Iterable
-
-
 class Node:
     def __init__(self,
                  left_child=None,
                  right_child=None,
                  mother=None,
-                 value=None,
-                 weight=None):
+                 value=None):
         self.left_child = left_child
         self.right_child = right_child
         self.mother = mother
         self.value = value
-        self.weight = weight
+        self.weight = self.get_and_update_weight_recursive()
 
     def set_children(self, left, right):
-        self.left_child = left
-        self.right_child = right
+        if self.right_child != right and self.left_child != left:
+            self.left_child = left
+            self.right_child = right
+            self.get_and_update_weight_recursive()
+        return
 
-    def get_weight(self, result=None):
-        if self.left_child is not None:
-            result += self.left_child.get_weight()
-        if self.right_child is not None:
-            result += self.right_child.get_weight()
-        return result + self.weight
+    def get_and_update_weight_recursive(self):
+        left = 0
+        right = 0
+
+        if self.left_child is not None and self.right_child is not None:
+            if self.left_child is not None:
+                left = self.left_child.get_and_update_weight_recursive()
+            if self.right_child is not None:
+                right = self.right_child.get_and_update_weight_recursive()
+            self.weight = left + right
+
+        return self.weight
+
+    def manipulate_weight(self, value):
+        self.weight += value
+        if self.mother is not None:
+            self.get_and_update_weight_recursive()
 
     def __repr__(self):
         return "%s - %s — %s _ %s" % (self.item, self.weight, self.left, self.right)
--- classes/tree.py
+++ classes/tree.py
@@ -1,7 +1,7 @@
 class Tree:
     def __init__(self, root = None):
         self.root = root
-        self.weight = self.root.get_weight()
+        self.weight = self.root.get_and_update_weight_recursive()
 
     def add_tree(self, tree):
         pass
