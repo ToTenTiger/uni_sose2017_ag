246bd8d2e17a1b0863c7ef7bc2a47a12686fcd27
r41l <stephanbrummel@online.de>
2017-05-04 16:34:40 +0200

    Updated Tree

--- classes/tree.py
+++ classes/tree.py
@@ -1,10 +1,30 @@
+from .node import Node
+
+
 class Tree:
-    def __init__(self, root = None):
+    def __init__(self, root: Node):
         self.root = root
-        self.weight = self.root.get_and_update_weight_recursive()
+        self.nnk = "0"
+        self.depth = 1
 
-    def add_tree(self, tree):
+    def add_node(self, node):
+        if not self.root.has_child():
+            self.root = node
+        else:
+            self.find_value()
+
+    def find_value(self, current_node=None, value=None):
+        result = False
+        if current_node is not None:
+            nd = current_node
+        else:
+            nd = self.root
+
+        if nd.has_child():
+            if nd.left_child is not None:
+                self.find_value(nd.left_child, value)
+            if result:
+                self.find_value(nd.right_child, value)
+
+    def delete_node(self, node):
         pass
-
-    def is_valid(self):
-        return self.root.is_valid
