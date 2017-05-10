17eee591e19c44b5e9664d940030cf6db303a8ba
ToTenTiger <bjoern.nowak@web.de>
2017-05-10 19:32:06 +0200

    add top sort method for kahn and tarjan

--- classes/di_graph.py
+++ classes/di_graph.py
@@ -21,14 +21,60 @@ class DiGraph(Graph):
         return self.dot
 
     # ----------------------------------------------------------------
+    # excise methods
+
+    def top_sort_kahn(self):
+        to_so = []
+        ohne_vorgaenger = []
+        ein_grade = {}
+
+        for node in self.get_nodes():
+            ein_grad = self.ein_grad(node)
+            if ein_grad > 0:
+                ein_grade[node] = ein_grad
+            else:
+                ohne_vorgaenger.append(node)
+
+        while ohne_vorgaenger:
+            current_node = ohne_vorgaenger.pop(0)
+            to_so.append(current_node)
+            for successor in self.get_neighbours_plus(current_node):
+                ein_grade[successor.node] -= 1
+                if ein_grade[successor.node] <= 0:
+                    ohne_vorgaenger.append(successor.node)
+                    ein_grade.pop(successor.node)
+
+        return to_so
+
+    def top_sort_tarjan(self):
+        to_so = []
+        visited = {}
+        ohne_nachfolger = []
+
+        for node in self.get_nodes():
+            visited[node] = False
+            aus_grad = self.aus_grad(node)
+            if aus_grad <= 0:
+                ohne_nachfolger.append(node)
+
+        for current_node in ohne_nachfolger:
+            self.top_sort_tarjan_visit(to_so, visited, current_node)
+
+        return to_so
+
+    def top_sort_tarjan_visit(self, to_so, visited, node):
+        if not visited[node]:
+            visited[node] = True
+            for ancestor in self.get_neighbours_minus(node):
+                self.top_sort_tarjan_visit(to_so, visited, ancestor)
+            to_so.append(node)
+
+    # ----------------------------------------------------------------
     # helper methods
 
     def find_msb(self):
         # TODO implement me
         exit("Not implemented yet")
-
-    # ----------------------------------------------------------------
-    # helper methods
 
     def kaZu_grad(self, node):
         return self.ein_grad(node)
