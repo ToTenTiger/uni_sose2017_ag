85cd712504acd865340b8e921c568347ac81405f
ToTenTiger <bjoern.nowak@web.de>
2017-05-11 19:25:43 +0200

    use os-specific file separator

--- tasks/e04_t17.py
+++ tasks/e04_t17.py
@@ -1,3 +1,5 @@
+from os.path import join as get_path
+
 from classes import Graph
 from . import create_print_render_dot
 from . import read_dot_body
@@ -7,7 +9,7 @@ print("Start: Exercise 04 Task 17")
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 graph = Graph(title="Exercise 04 Task 17 Graph 01",
-            filename="e04_t17_g01")
+              filename="e04_t17_g01")
 graph.read_input(value="a:b,h,e;" +
                        "b:g,c,e,a;" +
                        "c:h,b;" +
@@ -18,18 +20,15 @@ graph.read_input(value="a:b,h,e;" +
                        "h:c,a,f;;")
 dot = create_print_render_dot(graph)
 
+files = ["e04_t17_g02", "e04_t17_g03", "e04_t17_g04"]
+for i, file in enumerate(files, start=2):
+    create_print_render_dot(
+        Graph(title="Exercise 04 Task 17 Graph 0{}".format(i),
+              filename="e04_t17_g0{}".format(i),
+              body=read_dot_body(get_path("tasks", "e04_t17_g0{}.gv".format(i)))))
 
-create_print_render_dot(Graph(title="Exercise 04 Task 17 Graph 02",
-                              filename="e04_t17_g02",
-                              body=read_dot_body("tasks\e04_t17_g02.gv")))
-
-create_print_render_dot(Graph(title="Exercise 04 Task 17 Graph 03",
-                              filename="e04_t17_g03",
-                              body=read_dot_body("tasks\e04_t17_g03.gv")))
-
-create_print_render_dot(Graph(title="Exercise 04 Task 17 Graph 04",
-                              filename="e04_t17_g04",
-                              body=read_dot_body("tasks\e04_t17_g04.gv")))
+print("\nGraphs has been generated under directory 'graphs'." +
+      "\nFile name pattern e04_t17_g<NUMBER>\n")
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 print("End: Exercise 04 Task 17")
