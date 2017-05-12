c056cfe592345ba0ac4f188cc0917b5fec442279
ToTenTiger <bjoern-nowak@web.de>
2017-05-12 09:32:47 +0200

    no time to implement the method to build a structure graph :(

--- tasks/e05_t19.py
+++ tasks/e05_t19.py
@@ -1,4 +1,4 @@
-from classes import Graph
+from classes import DiGraph
 from . import create_print_render_dot
 from . import print_output_info
 
@@ -6,10 +6,16 @@ from . import print_output_info
 print("\nStart: Exercise 05 Task 19\n")
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-graph = Graph(title="Exercise 05 Task 19 Graph 01",
-              filename="e05_t19_g01")
+graph = DiGraph(title="Exercise 05 Task 19 Graph 01",
+                filename="e05_t19_g01")
 graph.read_input(value="a:f;b:a;c:a,e;d:b,c,f;e:b,g;f:b;g:c,f;;")
 dot = create_print_render_dot(graph)
+
+graph_strct = DiGraph(title="Exercise 05 Task 19 Graph 01",
+                      filename="e05_t19_g01_structur",
+                      allow_multi=True)
+graph_strct.read_input(value="A:;C:A,A,A;D:A,C;")
+dot_strct = create_print_render_dot(graph_strct)
 
 print_output_info(e="05", t="19")
 
