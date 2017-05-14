59f99ca1be9383a0fba1c248fa57a5951a938901
ToTenTiger <bjoern.nowak@web.de>
2017-05-14 12:08:39 +0200

    add util method for anonymous objects

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,4 +1,4 @@
-from .errors import *
+from .errors import FoundCircleError
 
 from .di_graph import DiGraph
 from .edge import Edge
@@ -9,3 +9,10 @@ from .heap_node import HeapNode
 from .request import Request
 
 __all__ = ["Edge", "Graph", "DiGraph", "Node", "HeapNode", "Request", "FoundCircleError"]
+
+
+def returnObject(values: dict):
+    obj = type('', (object,), values)()
+    # obj = type('', (object,), **kwargs)()
+    # obj = lambda **kwargs: type("Object", (), kwargs)()
+    return obj
